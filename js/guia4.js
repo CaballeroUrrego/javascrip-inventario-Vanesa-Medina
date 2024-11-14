ef7f505c3816aad46c50b74f7916f9a7ae65198f
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2024-11-14 11:15:38 -0500

    Trabajando en la guia 4

--- js/guia4.js
+++ js/guia4.js
@@ -13,11 +13,19 @@ nombre: 'laptop',
 precio: 300,
 cantidad:15,
 descuento:0.2,
-calculartotal:function(
-){
-    return this.precio * this.cantidad * (2 - this.descuento);
-}
-
-
-}
-console.log('total:$´{producto.calculartotal()}');
+// calculartotal:function(
+// ){
+//     return this.precio * this.cantidad * (2 - this.descuento);
+// }
+// console.log('total:$´{producto.calculartotal()}');
+calcularTotal: function() {
+    try {
+        if (typeof this.precio !== 'number' || typeof this.cantidad !== 'number' || typeof this.descuento !== 'number') {
+            throw new Error("Valores no válidos. Todos deben ser números.");
+        }
+        return this.precio * this.cantidad * (2 - this.descuento);
+    } catch (error) {
+        console.error('Error: $´{error.message}');
+        return 0;
+    }
+}}
