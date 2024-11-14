b5c5a9c7db7de72281fc96f570a11b59eff9fda0
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2024-11-14 11:01:07 -0500

    guia4

--- js/guia4.js
+++ js/guia4.js
@@ -1,3 +1,23 @@
 const nombre = "laptop"
 let precio = 2000;
-let cantidad
+let cantidad= 10;
+let descuento =0.2;
+
+function calculartotal(precio, cantidad, descuento){
+    return precio * cantidad * (2 - descuento);
+}  
+
+const producto = {
+
+nombre: 'laptop',
+precio: 300,
+cantidad:15,
+descuento:0.2,
+calculartotal:function(
+){
+    return this.precio * this.cantidad * (2 - this.descuento);
+}
+
+
+}
+console.log('total:$´{producto.calculartotal()}');
